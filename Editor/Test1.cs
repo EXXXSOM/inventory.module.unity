dcbaac31c00b34b069189bac92459b7ada59ab9e
EXXXSOM <57260754+EXXXSOM@users.noreply.github.com>
2021-04-04 19:39:22 +0300

    R.v1

--- Editor/Test1.cs
+++ Editor/Test1.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test1 : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace EXXXSOM.Inventory
+{
+    public class Test1 : MonoBehaviour
     {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
